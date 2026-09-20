dcfeea9324e7a2291edc52f36a1f132495820bd2
AhmadAdas21 <ahmadnajiadas20@gmail.com>
2026-09-20 22:02:15 +0300

    update the main code

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,8 +8,20 @@ namespace Task2
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Console.WriteLine("************************");
+            Console.WriteLine("Employee Management System");
+            Console.WriteLine("Choose one of the features below:");
+            Console.WriteLine("1 add employee");
+            Console.WriteLine("2 view all employees");
+            Console.WriteLine("3 search an employee");
+            Console.WriteLine("4 delet an employee");
+            Console.WriteLine("5 filter employees by department");
+            Console.WriteLine("6 sort employees by salary");
+            Console.ReadLine();
+
         }
     }
 }
